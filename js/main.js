81133a288838f5e4e28277a54761158ff4762de7
Sumer Mixon <sumer@ximm.us>
2017-10-10 15:48:50 -0400

    first steps

--- js/main.js
+++ js/main.js
@@ -8,35 +8,3 @@ function ItemsListViewModel(){
 
     self.test = "test";
 }
-
-/*
-jQuery Mobile 
-*/
-
-// Define a click binding for all anchors in the page
-$( "a" ).on( "click", function( event ) {
-
-    // Prevent the usual navigation behavior
-    event.preventDefault();
-
-    // Alter the url according to the anchor's href attribute, and
-    // store the data-foo attribute information with the url
-    $.mobile.navigate( $(this).attr( "href" ), {
-        foo: $(this).attr("data-foo")
-    });
-});
-
-// AJAX Success!
-// Fires everytime there's a successful ajax request completed. Useful to call functions that need to run on the newly rendered content.
-$(document).ajaxSuccess(function() {
-    
-    // Navigate to the data-log page on longhold of header bar.
-    $(function(){
-        $( "header" ).bind( "taphold", tapholdHandler ).disableSelection();
-        function tapholdHandler( event ){
-            event.preventDefault();
-            $.mobile.navigate("event-log.html", { transition: "flip"});
-        }
-    });
-
-}); // END ajaxSuccess()
